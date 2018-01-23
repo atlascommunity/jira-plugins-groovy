3c325f0afab216f25fc284b0a88ee20bd1e60640
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-01-23 13:45:17 +0300

    some work

---
+++ src/main/java/ru/mail/jira/plugins/groovy/api/entity/ScheduledTaskChangelog.java
@@ -0,0 +1,11 @@
+package ru.mail.jira.plugins.groovy.api.entity;
+
+import net.java.ao.schema.NotNull;
+import net.java.ao.schema.Table;
+
+@Table("S_TASK_CHANGELOG")
+public interface ScheduledTaskChangelog extends Changelog {
+    @NotNull
+    void setTask(ScheduledTask task);
+    ScheduledTask getTask();
+}
