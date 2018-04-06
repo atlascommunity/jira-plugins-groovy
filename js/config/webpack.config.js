7dcb579081c7bffcf7cc19d7e45b33080ff189b4
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-04-06 13:07:58 +0300

    clean up js build config

--- js/config/webpack.config.js
+++ js/config/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
@@ -15,9 +14,6 @@ const saveLicense = require('uglify-save-license');
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
-// Some apps do not use client-side routing with pushState.
-// For these, "homepage" can be set to "." to enable relative asset paths.
-const shouldUseRelativeAssetPaths = publicPath === './';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
@@ -256,9 +252,6 @@ module.exports = {
         }),
         // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
         extractLess,
-        new ManifestPlugin({
-            fileName: 'asset-manifest.json',
-        }),
         new CleanWebpackPlugin(
             cssDir + "*.*",
             {
