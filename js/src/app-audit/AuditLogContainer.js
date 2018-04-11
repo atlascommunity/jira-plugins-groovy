2f566934955df23b4da93fc12fc41c5548c51f5c
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-04-11 19:39:37 +0300

    make script column wider

--- js/src/app-audit/AuditLogContainer.js
+++ js/src/app-audit/AuditLogContainer.js
@@ -37,7 +37,7 @@ const tableHead = {
         },
         {
             content: AuditMessages.script,
-            width: '100px'
+            width: '200px'
         },
         {
             content: AuditMessages.description
