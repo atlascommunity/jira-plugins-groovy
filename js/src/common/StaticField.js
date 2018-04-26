767b2789436241d299e4e4c8e88d4dd6e3ca8981
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-04-26 14:28:16 +0300

    JD-121 added more types

--- js/src/common/StaticField.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import './StaticField.less';
-
-
-export function StaticFieldValue({children}) {
-    return <div className="static-field">{children}</div>;
-}
--- js/src/common/ak/StaticField.js
+++ js/src/common/ak/StaticField.js
@@ -1,11 +1,18 @@
 //@flow
-
 import * as React from 'react';
 
 import {Label} from '@atlaskit/field-base';
 
-import {StaticFieldValue} from '../StaticField';
+import './StaticField.less';
 
+
+type StaticFieldValueProps = {
+    children: React.Node
+}
+
+function StaticFieldValue({children}: StaticFieldValueProps): React.Node {
+    return <div className="static-field">{children}</div>;
+}
 
 type StaticFieldProps = {
     label: string,
