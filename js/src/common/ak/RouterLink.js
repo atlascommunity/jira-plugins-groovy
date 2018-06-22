4e8044b68657b594e0bcca1a7c0bdfdafcaeaa90
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-06-22 15:50:57 +0300

    fix type

--- js/src/common/ak/RouterLink.js
+++ js/src/common/ak/RouterLink.js
@@ -1,14 +1,14 @@
 //@flow
-import React, {type Element} from 'react';
+import React, {type Node} from 'react';
 import {Link} from 'react-router-dom';
 
 
 type Props = {
-    children: Element<any>,
+    children: Node,
     className: string,
     href: string,
-    onMouseEnter: (SyntheticEvent<any>) => void,
-    onMouseLeave: (SyntheticEvent<any>) => void,
+    onMouseEnter?: (SyntheticEvent<any>) => void,
+    onMouseLeave?: (SyntheticEvent<any>) => void,
 };
 
 export class RouterLink extends React.PureComponent<Props, {}> {
