7755aa6474ea22530fef4af5e1e9a8e9350b3cbb
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-07-02 14:56:48 +0300

    remove unused import

--- src/main/java/ru/mail/jira/plugins/groovy/impl/repository/JqlFunctionRepositoryImpl.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/repository/JqlFunctionRepositoryImpl.java
@@ -19,7 +19,6 @@ import ru.mail.jira.plugins.groovy.api.repository.ExecutionRepository;
 import ru.mail.jira.plugins.groovy.api.repository.JqlFunctionRepository;
 import ru.mail.jira.plugins.groovy.api.script.CompiledScript;
 import ru.mail.jira.plugins.groovy.api.service.ScriptService;
-import ru.mail.jira.plugins.groovy.api.service.admin.BuiltInScript;
 import ru.mail.jira.plugins.groovy.impl.AuditService;
 import ru.mail.jira.plugins.groovy.util.ChangelogHelper;
 import ru.mail.jira.plugins.groovy.util.ValidationException;
