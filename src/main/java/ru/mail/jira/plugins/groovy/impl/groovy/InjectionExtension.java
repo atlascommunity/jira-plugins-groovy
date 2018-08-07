feb9913257810d872264a7cdad478a22b8948255
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-08-07 19:23:50 +0300

    JD-297 support injection in script field

--- src/main/java/ru/mail/jira/plugins/groovy/impl/groovy/InjectionExtension.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/groovy/InjectionExtension.java
@@ -1,12 +1,6 @@
 package ru.mail.jira.plugins.groovy.impl.groovy;
 
-import org.apache.felix.framework.BundleWiringImpl;
-import org.codehaus.groovy.ast.AnnotationNode;
 import org.codehaus.groovy.ast.ClassNode;
-import org.codehaus.groovy.ast.expr.DeclarationExpression;
-import org.codehaus.groovy.ast.expr.VariableExpression;
-import org.codehaus.groovy.ast.stmt.ExpressionStatement;
-import org.codehaus.groovy.ast.stmt.Statement;
 import org.codehaus.groovy.classgen.GeneratorContext;
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.CompilePhase;
@@ -15,72 +9,25 @@ import org.codehaus.groovy.control.customizers.CompilationCustomizer;
 
 public class InjectionExtension extends CompilationCustomizer {
     private final ParseContextHolder parseContextHolder;
+    private final InjectionVisitor injectionVisitor;
 
     public InjectionExtension(ParseContextHolder parseContextHolder) {
         super(CompilePhase.CANONICALIZATION);
         this.parseContextHolder = parseContextHolder;
+        this.injectionVisitor = new InjectionVisitor(parseContextHolder);
     }
 
     @Override
     public void call(SourceUnit source, GeneratorContext context, ClassNode classNode) throws CompilationFailedException {
         ParseContext parseContext = parseContextHolder.get();
+
         if (parseContext.getCompletedExtensions().contains(InjectionExtension.class)) {
             return;
         }
 
-        for (Statement statement : source.getAST().getStatementBlock().getStatements()) {
-            if (statement instanceof ExpressionStatement) {
-                ExpressionStatement castedStatement = (ExpressionStatement) statement;
-                if (castedStatement.getExpression() instanceof DeclarationExpression) {
-                    DeclarationExpression expression = (DeclarationExpression) castedStatement.getExpression();
-
-                    if (expression.getLeftExpression() instanceof VariableExpression) {
-                        VariableExpression leftExpression = (VariableExpression) expression.getLeftExpression();
-
-                        for (AnnotationNode annotationNode : expression.getAnnotations()) {
-                            String annotationName = annotationNode.getClassNode().getName();
-                            boolean pluginModule = annotationName.equals("ru.mail.jira.plugins.groovy.api.script.PluginModule");
-                            boolean standardModule = annotationName.equals("ru.mail.jira.plugins.groovy.api.script.StandardModule");
-                            if (pluginModule || standardModule) {
-                                ClassNode type = leftExpression.getType();
-
-                                ScriptInjection injectionObject;
-
-                                String varName = leftExpression.getName();
-
-                                if (pluginModule) {
-                                    ClassLoader typeClassLoader = type.getTypeClass().getClassLoader();
-
-                                    if (typeClassLoader instanceof BundleWiringImpl.BundleClassLoader) {
-                                        BundleWiringImpl.BundleClassLoader castedClassLoader = (BundleWiringImpl.BundleClassLoader) typeClassLoader;
-
-                                        injectionObject = new ScriptInjection(
-                                            castedClassLoader.getBundle().getSymbolicName(),
-                                            type.getName(),
-                                            varName
-                                        );
-                                    } else {
-                                        throw new RuntimeException("Class is not from osgi bundle");
-                                    }
-                                } else {
-                                    injectionObject = new ScriptInjection(
-                                        null,
-                                        type.getName(),
-                                        varName
-                                    );
-                                }
-
-                                parseContext.getInjections().add(injectionObject);
-                                expression.setRightExpression(new VariableExpression(varName));
-
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+        source.getAST().getClasses().forEach(injectionVisitor::visitClass);
         parseContext.getCompletedExtensions().add(InjectionExtension.class);
     }
+
+
 }
---
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/groovy/InjectionVisitor.java
@@ -0,0 +1,88 @@
+package ru.mail.jira.plugins.groovy.impl.groovy;
+
+import org.apache.felix.framework.BundleWiringImpl;
+import org.codehaus.groovy.ast.*;
+import org.codehaus.groovy.ast.expr.*;
+import org.codehaus.groovy.control.SourceUnit;
+
+import java.util.Optional;
+
+public class InjectionVisitor extends ClassCodeVisitorSupport {
+    private final ParseContextHolder parseContextHolder;
+
+    public InjectionVisitor(ParseContextHolder parseContextHolder) {
+        this.parseContextHolder = parseContextHolder;
+    }
+
+    @Override
+    protected SourceUnit getSourceUnit() {
+        return null;
+    }
+
+    @Override
+    public void visitField(FieldNode node) {
+        super.visitField(node);
+
+        getInitialExpressionIfApplicable(node, node.getType(), node.getName())
+            .ifPresent(node::setInitialValueExpression);
+    }
+
+    @Override
+    public void visitDeclarationExpression(DeclarationExpression expression) {
+        super.visitDeclarationExpression(expression);
+
+        if (expression.getLeftExpression() instanceof VariableExpression) {
+            VariableExpression leftExpression = (VariableExpression) expression.getLeftExpression();
+
+            getInitialExpressionIfApplicable(expression, leftExpression.getType(), leftExpression.getName())
+                .ifPresent(expression::setRightExpression);
+        }
+    }
+
+    private Optional<Expression> getInitialExpressionIfApplicable(AnnotatedNode node, ClassNode type, String varName) {
+        for (AnnotationNode annotationNode : node.getAnnotations()) {
+            String annotationName = annotationNode.getClassNode().getName();
+            boolean pluginModule = annotationName.equals("ru.mail.jira.plugins.groovy.api.script.PluginModule");
+            boolean standardModule = annotationName.equals("ru.mail.jira.plugins.groovy.api.script.StandardModule");
+            if (pluginModule || standardModule) {
+                ScriptInjection injectionObject;
+
+                if (pluginModule) {
+                    ClassLoader typeClassLoader = type.getTypeClass().getClassLoader();
+
+                    if (typeClassLoader instanceof BundleWiringImpl.BundleClassLoader) {
+                        BundleWiringImpl.BundleClassLoader castedClassLoader = (BundleWiringImpl.BundleClassLoader) typeClassLoader;
+
+                        injectionObject = new ScriptInjection(
+                            castedClassLoader.getBundle().getSymbolicName(),
+                            type.getName(),
+                            varName
+                        );
+                    } else {
+                        throw new RuntimeException("Class is not from osgi bundle");
+                    }
+                } else {
+                    injectionObject = new ScriptInjection(
+                        null,
+                        type.getName(),
+                        varName
+                    );
+                }
+
+                parseContextHolder.get().getInjections().add(injectionObject);
+
+                return Optional.of(buildInitialExpression(varName));
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private Expression buildInitialExpression(String varName) {
+        return new MethodCallExpression(
+            new PropertyExpression(new VariableExpression("this"), "binding"),
+            "getVariable",
+            new ArgumentListExpression(new ConstantExpression(varName))
+        );
+    }
+}
