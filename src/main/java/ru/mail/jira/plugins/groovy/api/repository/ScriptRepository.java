efb2ce0b8f5bcdd26b7bc79ef1edabade91194e0
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-09-27 17:30:28 +0300

    JD-342 move all transactional database operations of registry to dao

--- src/main/java/ru/mail/jira/plugins/groovy/api/repository/ScriptRepository.java
+++ src/main/java/ru/mail/jira/plugins/groovy/api/repository/ScriptRepository.java
@@ -1,6 +1,5 @@
 package ru.mail.jira.plugins.groovy.api.repository;
 
-import com.atlassian.activeobjects.tx.Transactional;
 import com.atlassian.jira.user.ApplicationUser;
 import ru.mail.jira.plugins.groovy.api.dto.*;
 import ru.mail.jira.plugins.groovy.api.dto.directory.*;
@@ -20,35 +19,26 @@ public interface ScriptRepository {
 
     List<ScriptDescription> getAllScriptDescriptions(WorkflowScriptType type);
 
-    @Transactional
     ScriptDirectoryDto createDirectory(ApplicationUser user, ScriptDirectoryForm form);
 
-    @Transactional
     ScriptDirectoryDto updateDirectory(ApplicationUser user, int id, ScriptDirectoryForm form);
 
-    @Transactional
     void deleteDirectory(ApplicationUser user, int id);
 
-    @Transactional
     void restoreDirectory(ApplicationUser user, int id);
 
     void moveDirectory(ApplicationUser user, int id, ParentForm form);
 
     RegistryScriptDto getScript(int id, boolean includeChangelogs, boolean expandName, boolean includeErrorCount);
 
-    @Transactional
     RegistryScriptDto createScript(ApplicationUser user, RegistryScriptForm scriptForm);
 
-    @Transactional
     RegistryScriptDto updateScript(ApplicationUser user, int id, RegistryScriptForm scriptForm);
 
-    @Transactional
     void moveScript(ApplicationUser user, int id, ParentForm form);
 
-    @Transactional
     void deleteScript(ApplicationUser user, int id);
 
-    @Transactional
     void restoreScript(ApplicationUser user, int id);
 
     List<ChangelogDto> getScriptChangelogs(int id);
--- src/main/java/ru/mail/jira/plugins/groovy/impl/repository/ScriptRepositoryImpl.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/repository/ScriptRepositoryImpl.java
@@ -7,12 +7,11 @@ import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.I18nHelper;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsDevService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import net.java.ao.DBParam;
 import net.java.ao.Query;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.mail.jira.plugins.groovy.impl.AuditService;
+import ru.mail.jira.plugins.groovy.api.dao.RegistryDao;
 import ru.mail.jira.plugins.groovy.api.dto.PickerOption;
 import ru.mail.jira.plugins.groovy.util.*;
 import ru.mail.jira.plugins.groovy.api.dto.workflow.WorkflowScriptType;
@@ -20,14 +19,11 @@ import ru.mail.jira.plugins.groovy.api.repository.ExecutionRepository;
 import ru.mail.jira.plugins.groovy.api.repository.ScriptRepository;
 import ru.mail.jira.plugins.groovy.api.service.ScriptService;
 import ru.mail.jira.plugins.groovy.api.dto.*;
-import ru.mail.jira.plugins.groovy.api.entity.EntityType;
 import ru.mail.jira.plugins.groovy.api.dto.directory.*;
 import ru.mail.jira.plugins.groovy.api.entity.*;
-import ru.mail.jira.plugins.groovy.api.service.WatcherService;
 import ru.mail.jira.plugins.groovy.impl.ScriptInvalidationService;
 import ru.mail.jira.plugins.groovy.api.script.ParseContext;
 
-import java.sql.Timestamp;
 import java.text.Collator;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -41,12 +37,11 @@ public class ScriptRepositoryImpl implements ScriptRepository {
     private final ClusterLockService clusterLockService;
     private final ActiveObjects ao;
     private final ScriptInvalidationService scriptInvalidationService;
+    private final RegistryDao registryDao;
     private final ScriptService scriptService;
     private final JsonMapper jsonMapper;
     private final ExecutionRepository executionRepository;
     private final ChangelogHelper changelogHelper;
-    private final WatcherService watcherService;
-    private final AuditService auditService;
 
     @Autowired
     public ScriptRepositoryImpl(
@@ -54,39 +49,21 @@ public class ScriptRepositoryImpl implements ScriptRepository {
         @ComponentImport ClusterLockService clusterLockService,
         @ComponentImport ActiveObjects ao,
         ScriptInvalidationService scriptInvalidationService,
+        RegistryDao registryDao,
         ScriptService scriptService,
         JsonMapper jsonMapper,
         ExecutionRepository executionRepository,
-        ChangelogHelper changelogHelper,
-        WatcherService watcherService,
-        AuditService auditService
+        ChangelogHelper changelogHelper
     ) {
         this.i18nHelper = i18nHelper;
         this.clusterLockService = clusterLockService;
         this.ao = ao;
         this.scriptInvalidationService = scriptInvalidationService;
+        this.registryDao = registryDao;
         this.scriptService = scriptService;
         this.jsonMapper = jsonMapper;
         this.executionRepository = executionRepository;
         this.changelogHelper = changelogHelper;
-        this.watcherService = watcherService;
-        this.auditService = auditService;
-    }
-
-    private ScriptDirectory getParentDirectory(Integer parentId) {
-        ScriptDirectory parentDirectory = null;
-
-        if (parentId != null) {
-            parentDirectory = ao.get(ScriptDirectory.class, parentId);
-
-            if (parentDirectory == null) {
-                throw new IllegalArgumentException(i18nHelper.getText("ru.mail.jira.plugins.groovy.error.directoryNotFound", parentId));
-            } else if (parentDirectory.isDeleted()) {
-                throw new IllegalArgumentException(i18nHelper.getText("ru.mail.jira.plugins.groovy.error.parentDirectoryIsDeleted"));
-            }
-        }
-
-        return parentDirectory;
     }
 
     @Override
@@ -144,91 +121,29 @@ public class ScriptRepositoryImpl implements ScriptRepository {
     public ScriptDirectoryDto createDirectory(ApplicationUser user, ScriptDirectoryForm form) {
         validateDirectoryForm(form);
 
-        ScriptDirectory directory = ao.create(
-            ScriptDirectory.class,
-            new DBParam("NAME", form.getName()),
-            new DBParam("PARENT_ID", getParentDirectory(form.getParentId())),
-            new DBParam("DELETED", false)
-        );
-
-        addAuditLogAndNotify(user, EntityAction.CREATED, directory, directory.getID() + " - " + directory.getName());
-
-        return buildDirectoryDto(directory);
+        return buildDirectoryDto(registryDao.createDirectory(user, form));
     }
 
     @Override
     public ScriptDirectoryDto updateDirectory(ApplicationUser user, int id, ScriptDirectoryForm form) {
         validateDirectoryForm(form);
 
-        ScriptDirectory directory = ao.get(ScriptDirectory.class, id);
-        directory.setName(form.getName());
-        directory.setParent(getParentDirectory(form.getParentId()));
-        directory.save();
-
-        addAuditLogAndNotify(user, EntityAction.UPDATED, directory, directory.getID() + " - " + directory.getName());
-
-        return buildDirectoryDto(directory);
+        return buildDirectoryDto(registryDao.updateDirectory(user, id, form));
     }
 
     @Override
     public void deleteDirectory(ApplicationUser user, int id) {
-        deleteDirectory(user, ao.get(ScriptDirectory.class, id));
+        registryDao.deleteDirectory(user, id);
     }
 
     @Override
     public void restoreDirectory(ApplicationUser user, int id) {
-        restoreDirectory(user, ao.get(ScriptDirectory.class, id));
-    }
-
-    private void deleteDirectory(ApplicationUser user, ScriptDirectory directory) {
-        directory.setDeleted(true);
-        directory.save();
-
-        for (ScriptDirectory child : getChildren(directory)) {
-            deleteDirectory(user, child);
-        }
-
-        for (Script script : getScripts(directory)) {
-            deleteScript(user, script);
-        }
-
-        addAuditLogAndNotify(user, EntityAction.DELETED, directory, directory.getID() + " - " + directory.getName());
-    }
-
-    private void restoreDirectory(ApplicationUser user, ScriptDirectory directory) {
-        directory.setDeleted(false);
-        directory.save();
-
-        ScriptDirectory parent = directory.getParent();
-        if (parent != null && parent.isDeleted()) {
-            restoreDirectory(user, parent);
-        }
-
-        addAuditLogAndNotify(user, EntityAction.RESTORED, directory, directory.getID() + " - " + directory.getName());
+        registryDao.restoreDirectory(user, id);
     }
 
     @Override
     public void moveDirectory(ApplicationUser user, int id, ParentForm form) {
-        ScriptDirectory directory = ao.get(ScriptDirectory.class, id);
-        ScriptDirectory oldParent = directory.getParent();
-        ScriptDirectory newParent = null;
-
-        if (form.getParentId() != null) {
-            newParent = ao.get(ScriptDirectory.class, form.getParentId());
-        }
-
-        directory.setParent(newParent);
-        directory.save();
-
-        addAuditLogAndNotify(user, EntityAction.MOVED, directory, getName(oldParent) + " -> " + getName(newParent));
-    }
-
-    private void addAuditLogAndNotify(ApplicationUser user, EntityAction action, ScriptDirectory directory, String description) {
-        auditService.addAuditLogAndNotify(
-            user, action,
-            EntityType.REGISTRY_DIRECTORY, directory.getID(), directory.getName(),
-            null, null, description, getWatchers(directory)
-        );
+        registryDao.moveDirectory(user, id, form);
     }
 
     @Override
@@ -254,33 +169,9 @@ public class ScriptRepositoryImpl implements ScriptRepository {
         }
 
         ParseContext parseContext = validateScriptForm(true, scriptForm);
-
         String parameters = parseContext.getParameters().size() > 0 ? jsonMapper.write(parseContext.getParameters()) : null;
 
-        Script script = ao.create(
-            Script.class,
-            new DBParam("NAME", scriptForm.getName()),
-            new DBParam("UUID", UUID.randomUUID().toString()),
-            new DBParam("DESCRIPTION", scriptForm.getDescription()),
-            new DBParam("SCRIPT_BODY", scriptForm.getScriptBody()),
-            new DBParam("DIRECTORY_ID", scriptForm.getDirectoryId()),
-            new DBParam("DELETED", false),
-            new DBParam("PARAMETERS", parameters),
-            new DBParam("TYPES", scriptForm.getTypes().stream().map(WorkflowScriptType::name).collect(Collectors.joining(",")))
-        );
-
-        String diff = changelogHelper.generateDiff(script.getID(), "", script.getName(), "", scriptForm.getScriptBody());
-
-        String comment = scriptForm.getComment();
-        if (comment == null) {
-            comment = Const.CREATED_COMMENT;
-        }
-
-        changelogHelper.addChangelog(Changelog.class, script.getID(), user.getKey(), diff, comment);
-
-        addAuditLogAndNotify(user, EntityAction.CREATED, script, diff, comment);
-
-        return buildScriptDto(script, true, false, true, true);
+        return buildScriptDto(registryDao.createScript(user, scriptForm, parameters), true, false, true, true);
     }
 
     @Override
@@ -301,49 +192,7 @@ public class ScriptRepositoryImpl implements ScriptRepository {
 
     @Override
     public void moveScript(ApplicationUser user, int id, ParentForm form) {
-        Script script = ao.get(Script.class, id);
-        ScriptDirectory oldParent = script.getDirectory();
-        ScriptDirectory newParent = null;
-
-        if (form.getParentId() != null) {
-            newParent = ao.get(ScriptDirectory.class, form.getParentId());
-        }
-
-        script.setDirectory(newParent);
-        script.save();
-
-        addAuditLogAndNotify(user, EntityAction.MOVED, script, null, getName(oldParent) + " -> " + getName(newParent));
-    }
-
-    private RegistryScriptDto doUpdateScript(ApplicationUser user, int id, RegistryScriptForm form, ParseContext parseContext) {
-        Script script = ao.get(Script.class, id);
-
-        if (script.isDeleted()) {
-            throw new IllegalArgumentException("Script " + id + " is deleted");
-        }
-
-        String diff = changelogHelper.generateDiff(id, script.getName(), form.getName(), script.getScriptBody(), form.getScriptBody());
-        String comment = form.getComment();
-
-        changelogHelper.addChangelog(Changelog.class, script.getID(), user.getKey(), diff, comment);
-
-        String parameters = parseContext.getParameters().size() > 0 ? jsonMapper.write(parseContext.getParameters()) : null;
-
-        script.setName(form.getName());
-        script.setUuid(UUID.randomUUID().toString());
-        script.setDescription(form.getDescription());
-        script.setScriptBody(form.getScriptBody());
-        script.setParameters(parameters);
-        script.setTypes(form.getTypes().stream().map(WorkflowScriptType::name).collect(Collectors.joining(",")));
-        script.save();
-
-        addAuditLogAndNotify(user, EntityAction.UPDATED, script, diff, comment);
-
-        if (!diff.isEmpty()) {
-            executionRepository.deleteExecutions(id, new Timestamp(System.currentTimeMillis()));
-        }
-
-        return buildScriptDto(script, true, false, true, true);
+        registryDao.moveScript(user, id, form);
     }
 
     @Override
@@ -352,7 +201,7 @@ public class ScriptRepositoryImpl implements ScriptRepository {
 
         lock.lock();
         try {
-            deleteScript(user, ao.get(Script.class, id));
+            registryDao.deleteScript(user, id);
         } finally {
             lock.unlock();
         }
@@ -364,7 +213,7 @@ public class ScriptRepositoryImpl implements ScriptRepository {
 
         lock.lock();
         try {
-            restoreScript(user, ao.get(Script.class, id));
+            registryDao.restoreScript(user, id);
         } finally {
             lock.unlock();
         }
@@ -375,24 +224,10 @@ public class ScriptRepositoryImpl implements ScriptRepository {
         return changelogHelper.collect(ao.find(Changelog.class, Query.select().where("SCRIPT_ID = ?", id)));
     }
 
-    private void deleteScript(ApplicationUser user, Script script) {
-        script.setDeleted(true);
-        script.save();
+    private RegistryScriptDto doUpdateScript(ApplicationUser user, int id, RegistryScriptForm form, ParseContext parseContext) {
+        String parameters = parseContext.getParameters().size() > 0 ? jsonMapper.write(parseContext.getParameters()) : null;
 
-        addAuditLogAndNotify(user, EntityAction.DELETED, script, null, script.getID() + " - " + script.getName());
-    }
-
-    private void restoreScript(ApplicationUser user, Script script) {
-        script.setDeleted(false);
-        script.save();
-
-        restoreDirectory(user, script.getDirectory());
-
-        addAuditLogAndNotify(user, EntityAction.RESTORED, script, null, script.getID() + " - " + script.getName());
-    }
-
-    private void addAuditLogAndNotify(ApplicationUser user, EntityAction action, Script script, String diff, String description) {
-        auditService.addAuditLogAndNotify(user, action, EntityType.REGISTRY_SCRIPT, script, diff, description, getWatchers(script));
+        return buildScriptDto(registryDao.updateScript(user, id, form, parameters), true, false, true, true);
     }
 
     private RegistryScriptDto buildScriptDto(Script script, boolean includeChangelogs, boolean expandName, boolean includeParentName, boolean includeErrorCount) {
@@ -502,47 +337,7 @@ public class ScriptRepositoryImpl implements ScriptRepository {
         return result;
     }
 
-    private List<ApplicationUser> getWatchers(Script script) {
-        List<ApplicationUser> watchers = getWatchers(script.getDirectory());
-
-        watchers.addAll(watcherService.getWatchers(EntityType.REGISTRY_SCRIPT, script.getID()));
-
-        return watchers;
-    }
-
-    private List<ApplicationUser> getWatchers(ScriptDirectory directory) {
-        List<ApplicationUser> watchers = new ArrayList<>();
-
-        while (directory != null) {
-            watchers.addAll(watcherService.getWatchers(EntityType.REGISTRY_DIRECTORY, directory.getID()));
-
-            directory = directory.getParent();
-        }
-
-        return watchers;
-    }
-
-    private ScriptDirectory[] getChildren(ScriptDirectory directory) {
-        return ao
-            .find(
-                ScriptDirectory.class,
-                Query.select().where("DELETED = ? AND PARENT_ID = ?", Boolean.FALSE, directory.getID())
-            );
-    }
-
-    private Script[] getScripts(ScriptDirectory directory) {
-        return ao
-            .find(
-                Script.class,
-                Query.select().where("DELETED = ? AND DIRECTORY_ID = ?", Boolean.FALSE, directory.getID())
-            );
-    }
-
     private static String getLockKey(int id) {
         return ScriptRepositoryImpl.class.toString() + "_script_" + id;
-    }
-
-    private static String getName(ScriptDirectory directory) {
-        return directory != null ? (directory.getName() + "(" + directory.getID() + ")") : null;
     }
 }
