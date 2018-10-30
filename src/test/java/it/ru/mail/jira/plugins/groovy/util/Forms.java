d96387be2adc349c047ff8125d0088d508879a7a
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-10-30 17:45:39 +0300

    JD-16 add test case

---
+++ src/test/java/it/ru/mail/jira/plugins/groovy/util/Forms.java
@@ -0,0 +1,21 @@
+package it.ru.mail.jira.plugins.groovy.util;
+
+import ru.mail.jira.plugins.groovy.api.dto.global.GlobalObjectForm;
+import ru.mail.jira.plugins.groovy.impl.FileUtil;
+
+import java.io.IOException;
+
+public final class Forms {
+    private Forms() {}
+
+    public static GlobalObjectForm globalObject(String sourcePath) throws IOException {
+        long ts = System.currentTimeMillis();
+        String globalObjectName = "testObject" + ts;
+
+        GlobalObjectForm form = new GlobalObjectForm();
+        form.setName(globalObjectName);
+        form.setScriptBody(FileUtil.readArquillianExample(sourcePath));
+
+        return form;
+    }
+}
