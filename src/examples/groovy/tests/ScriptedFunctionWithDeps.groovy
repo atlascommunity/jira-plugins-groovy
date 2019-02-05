65d9715af26f96c264b3a1ae13926e802086b55c
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2019-02-05 16:56:06 +0300

    update test class

--- src/examples/groovy/tests/ScriptedFunctionWithDeps.groovy
+++ src/examples/groovy/tests/ScriptedFunctionWithDeps.groovy
@@ -2,7 +2,6 @@ import com.atlassian.jira.JiraDataType
 import com.atlassian.jira.JiraDataTypes
 import com.atlassian.jira.issue.index.DocumentConstants
 import com.atlassian.jira.issue.search.SearchProviderFactory
-import com.atlassian.jira.issue.search.filters.IssueIdFilter
 import com.atlassian.jira.jql.query.QueryCreationContext
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.util.MessageSet
@@ -10,12 +9,12 @@ import com.atlassian.jira.util.MessageSetImpl
 import com.atlassian.query.clause.TerminalClause
 import com.atlassian.query.operand.FunctionOperand
 import org.apache.lucene.index.Term
-import org.apache.lucene.search.ConstantScoreQuery
 import org.apache.lucene.search.Query
 import org.apache.lucene.search.TermQuery
 import ru.mail.jira.plugins.groovy.api.jql.ScriptedJqlQueryFunction
 import ru.mail.jira.plugins.groovy.api.script.StandardModule
 import ru.mail.jira.plugins.groovy.util.lucene.IssueIdCollector
+import ru.mail.jira.plugins.groovy.util.lucene.QueryUtil
 
 import javax.annotation.Nonnull
 
@@ -45,11 +44,12 @@ class TestJqlValuesFunction implements ScriptedJqlQueryFunction {
         return JiraDataTypes.USER
     }
 
+    @Override
     Query getQuery(@Nonnull QueryCreationContext queryCreationContext, @Nonnull FunctionOperand operand) {
         IssueIdCollector collector = new IssueIdCollector()
         searchProviderFactory
                 .getSearcher(SearchProviderFactory.ISSUE_INDEX)
                 .search(new TermQuery(new Term(DocumentConstants.ISSUE_ASSIGNEE, queryCreationContext.applicationUser.key)), collector)
-        return new ConstantScoreQuery(new IssueIdFilter(collector.getIssueIds()))
+        return QueryUtil.createIssueIdQuery(collector.getIssueIds())
     }
 }
