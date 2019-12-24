09a6d9bcda030ca61d969aa7766aac592a3b967e
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2019-12-24 13:52:23 +0300

    #45 add script for code search

--- src/main/java/ru/mail/jira/plugins/groovy/impl/repository/querydsl/QFieldConfig.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/repository/querydsl/QFieldConfig.java
@@ -8,6 +8,7 @@ public class QFieldConfig extends EnhancedRelationalPathBase<QFieldConfig> {
     public final NumberPath<Integer> ID = createIntegerCol("ID").asPrimaryKey().build();
     public final StringPath UUID = createStringCol("UUID").build();
     public final NumberPath<Integer> FIELD_CONFIG_ID = createIntegerCol("FIELD_CONFIG_ID").build();
+    public final StringPath SCRIPT_BODY = createStringCol("SCRIPT_BODY").build();
 
     public QFieldConfig() {
         super(QFieldConfig.class, "AO_2FC5DA_FIELD_CONFIG");
