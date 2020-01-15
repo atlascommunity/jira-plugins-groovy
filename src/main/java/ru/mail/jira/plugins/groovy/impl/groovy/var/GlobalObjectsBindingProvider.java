728dd67de86ddf8af92b9831b339c526355eaa67
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2020-01-15 18:46:37 +0300

    JD-818 use threadlocal state during initialization

--- src/main/java/ru/mail/jira/plugins/groovy/impl/groovy/var/GlobalObjectsBindingProvider.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/groovy/var/GlobalObjectsBindingProvider.java
@@ -34,9 +34,8 @@ public class GlobalObjectsBindingProvider implements BindingProvider, PluginLife
     private final Logger logger = LoggerFactory.getLogger(GlobalObjectsBindingProvider.class);
 
     private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
-    private volatile Map<String, BindingDescriptor<?>> objects = ImmutableMap.of();
-    private volatile Map<String, Class> types = ImmutableMap.of();
-    private volatile boolean initialized = false;
+    private final ThreadLocal<GlobalObjectsState> tempState = new ThreadLocal<>();
+    private volatile GlobalObjectsState state = null;
 
     private final ScriptService scriptService;
     private final GlobalObjectDao globalObjectDao;
@@ -79,115 +78,120 @@ public class GlobalObjectsBindingProvider implements BindingProvider, PluginLife
     }
 
     private void unsafeLoadCaches() {
-        ClassLoaderUtil.runInContext(() -> {
-            logger.info("loading cache");
+        try {
+            GlobalObjectsState state = new GlobalObjectsState();
+            tempState.set(state);
 
-            objects
-                .values()
-                .stream()
-                .map(BindingDescriptor::getType)
-                .forEach(InvokerHelper::removeClass);
+            ClassLoaderUtil.runInContext(() -> {
+                logger.info("loading cache");
 
-            this.objects = new HashMap<>();
-            this.types = new HashMap<>();
-            this.initialized = true;
+                state
+                    .objects
+                    .values()
+                    .stream()
+                    .map(BindingDescriptor::getType)
+                    .forEach(InvokerHelper::removeClass);
 
-            boolean incomplete = false;
-            List<GlobalObject> allObjects = new LinkedList<>(globalObjectDao.getAll());
-            int prevRemaining = allObjects.size();
+                boolean incomplete = false;
+                List<GlobalObject> allObjects = new LinkedList<>(globalObjectDao.getAll());
+                int prevRemaining = allObjects.size();
 
-            while (true) {
-                allObjectsLoop:
-                for (Iterator<GlobalObject> iterator = allObjects.iterator(); iterator.hasNext(); ) {
-                    GlobalObject globalObject = iterator.next();
+                while (true) {
+                    allObjectsLoop:
+                    for (Iterator<GlobalObject> iterator = allObjects.iterator(); iterator.hasNext(); ) {
+                        GlobalObject globalObject = iterator.next();
 
-                    //check if all dependencies are initialized
-                    String dependenciesString = globalObject.getDependencies();
-                    if (dependenciesString != null) {
-                        String[] dependencies = dependenciesString.split(";");
+                        //check if all dependencies are initialized
+                        String dependenciesString = globalObject.getDependencies();
+                        if (dependenciesString != null) {
+                            String[] dependencies = dependenciesString.split(";");
 
-                        for (String dependency : dependencies) {
-                            if (!types.containsKey(dependency)) {
-                                logger.warn("unable to satisfy dependency on {} for {}", dependency, globalObject.getName());
-                                continue allObjectsLoop;
+                            for (String dependency : dependencies) {
+                                if (!state.types.containsKey(dependency)) {
+                                    logger.warn("unable to satisfy dependency on {} for {}", dependency, globalObject.getName());
+                                    continue allObjectsLoop;
+                                }
                             }
                         }
-                    }
 
-                    try {
-                        long t = System.currentTimeMillis();
+                        try {
+                            long t = System.currentTimeMillis();
 
-                        CompiledScript<?> compiledScript = scriptService.parseSingleton(globalObject.getScriptBody(), true, ImmutableMap.of());
-                        Class<?> objectClass = compiledScript.getScriptClass();
-                        Object object = singletonFactory.createInstance(compiledScript);
+                            CompiledScript<?> compiledScript = scriptService.parseSingleton(globalObject.getScriptBody(), true, ImmutableMap.of());
+                            Class<?> objectClass = compiledScript.getScriptClass();
+                            Object object = singletonFactory.createInstance(compiledScript);
 
-                        objects.put(
-                            globalObject.getName(),
-                            new LazyDocBindingDescriptorImpl(object, object.getClass(), () -> {
-                                try {
-                                    return groovyDocService.parseDocs(objectClass.getCanonicalName(), objectClass.getSimpleName(), globalObject.getScriptBody());
-                                } catch (Exception e) {
-                                    logger.error("Unable to parse doc for global object {}", globalObject.getName(), e);
-                                    return null;
-                                }
-                            })
-                        );
-                        types.put(objectClass.getName(), objectClass);
+                            state.objects.put(
+                                globalObject.getName(),
+                                new LazyDocBindingDescriptorImpl(object, object.getClass(), () -> {
+                                    try {
+                                        return groovyDocService.parseDocs(objectClass.getCanonicalName(), objectClass.getSimpleName(), globalObject.getScriptBody());
+                                    } catch (Exception e) {
+                                        logger.error("Unable to parse doc for global object {}", globalObject.getName(), e);
+                                        return null;
+                                    }
+                                })
+                            );
+                            state.types.put(objectClass.getName(), objectClass);
 
-                        t = System.currentTimeMillis() - t;
+                            t = System.currentTimeMillis() - t;
 
-                        if (t >= ExecutionRepository.WARNING_THRESHOLD) {
+                            if (t >= ExecutionRepository.WARNING_THRESHOLD) {
+                                executionRepository.trackInline(
+                                    globalObject.getUuid(),
+                                    t,
+                                    true,
+                                    null,
+                                    ImmutableMap.of(
+                                        "type", ScriptType.GLOBAL_OBJECT.name()
+                                    )
+                                );
+                            }
+
+                            iterator.remove();
+                        } catch (Exception e) {
+                            logger.error("Unable to initialize global object {}", globalObject.getName(), e);
                             executionRepository.trackInline(
                                 globalObject.getUuid(),
-                                t,
-                                true,
-                                null,
+                                0L,
+                                false,
+                                e,
                                 ImmutableMap.of(
                                     "type", ScriptType.GLOBAL_OBJECT.name()
                                 )
                             );
                         }
-
-                        iterator.remove();
-                    } catch (Exception e) {
-                        logger.error("Unable to initialize global object {}", globalObject.getName(), e);
-                        executionRepository.trackInline(
-                            globalObject.getUuid(),
-                            0L,
-                            false,
-                            e,
-                            ImmutableMap.of(
-                                "type", ScriptType.GLOBAL_OBJECT.name()
-                            )
-                        );
                     }
+
+                    //if nothing is initialized in last iteration, consider remaining objects as failed
+                    if (prevRemaining == allObjects.size()) {
+                        if (allObjects.size() > 0) {
+                            logger.error(
+                                "Failed to initialize objects: {}",
+                                allObjects.stream().map(GlobalObject::getName).collect(Collectors.joining(";"))
+                            );
+                        }
+                        break;
+                    }
+
+                    prevRemaining = allObjects.size();
                 }
 
-                //if nothing is initialized in last iteration, consider remaining objects as failed
-                if (prevRemaining == allObjects.size()) {
-                    if (allObjects.size() > 0) {
-                        logger.error(
-                            "Failed to initialize objects: {}",
-                            allObjects.stream().map(GlobalObject::getName).collect(Collectors.joining(";"))
-                        );
-                    }
-                    break;
-                }
+                logger.info("loaded global object cache (incomplete: {})", incomplete);
+                this.state = state;
 
-                prevRemaining = allObjects.size();
-            }
-
-            logger.info("loaded global object cache (incomplete: {})", incomplete);
-
-            return null;
-        });
+                return null;
+            });
+        } finally {
+            tempState.remove();
+        }
     }
 
     private void initializePrematurely() {
         Lock wLock = rwLock.writeLock();
         wLock.lock();
         try {
-            if (initialized) {
+            if (state != null) {
                 return;
             }
 
@@ -200,30 +204,50 @@ public class GlobalObjectsBindingProvider implements BindingProvider, PluginLife
 
     @Override
     public Map<String, BindingDescriptor<?>> getBindings() {
-        if (!initialized) {
+        GlobalObjectsState tempState = this.tempState.get();
+        if (tempState != null) {
+            return tempState.objects;
+        }
+
+        if (state == null) {
             initializePrematurely();
         }
 
         Lock rLock = rwLock.readLock();
         rLock.lock();
         try {
-            return this.objects;
+            return state.objects;
         } finally {
             rLock.unlock();
         }
     }
 
     public Map<String, Class> getTypes() {
-        return types;
+        GlobalObjectsState tempState = this.tempState.get();
+        if (tempState != null) {
+            return tempState.types;
+        }
+
+        if (state == null) {
+            initializePrematurely();
+        }
+
+        Lock rLock = rwLock.readLock();
+        rLock.lock();
+        try {
+            return state.types;
+        } finally {
+            rLock.unlock();
+        }
     }
 
     @Override
     public void onStart() {
-        if (!initialized) {
+        if (state == null) {
             ReentrantReadWriteLock.WriteLock wLock = rwLock.writeLock();
             wLock.lock();
             try {
-                if (!initialized) {
+                if (state == null) {
                     unsafeLoadCaches();
                 }
             } finally {
@@ -237,7 +261,8 @@ public class GlobalObjectsBindingProvider implements BindingProvider, PluginLife
         Lock wLock = rwLock.writeLock();
         wLock.lock();
         try {
-            this.objects
+            this.state
+                .objects
                 .values()
                 .stream()
                 .map(BindingDescriptor::getType)
@@ -256,8 +281,11 @@ public class GlobalObjectsBindingProvider implements BindingProvider, PluginLife
      * UNSAFE - for tests only
      */
     public void deinitialize() {
-        this.initialized = false;
-        this.objects = ImmutableMap.of();
-        this.types = ImmutableMap.of();
+        this.state = null;
+    }
+
+    private static class GlobalObjectsState {
+        private final Map<String, BindingDescriptor<?>> objects = new HashMap<>();
+        private final Map<String, Class> types = new HashMap<>();
     }
 }
