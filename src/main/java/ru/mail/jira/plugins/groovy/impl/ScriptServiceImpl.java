8b0fb9d0edc634d4e35e4eca3d2a25444f49446d
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2020-09-23 18:28:45 +0300

    script cache

--- src/main/java/ru/mail/jira/plugins/groovy/impl/ScriptServiceImpl.java
+++ src/main/java/ru/mail/jira/plugins/groovy/impl/ScriptServiceImpl.java
@@ -53,8 +53,6 @@ public class ScriptServiceImpl implements ScriptService, PluginLifecycleAware {
     private final List<BindingProvider> bindingProviders = new ArrayList<>();
     private final Cache<String, CompiledScript> scriptCache = Caffeine
         .newBuilder()
-        .maximumSize(1000)
-        .expireAfterAccess(1, TimeUnit.HOURS)
         .recordStats()
         .removalListener((String k, CompiledScript v, RemovalCause reason) -> {
             if (v != null) {
